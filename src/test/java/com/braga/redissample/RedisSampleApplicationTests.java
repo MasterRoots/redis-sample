714baa0cacf7ff11d6a5ecb0b88e73963212e3bb
Rodrigo Braga <rodrigob.braga@gmail.com>
2022-02-22 16:31:14 -0300

    first commit

---
+++ src/test/java/com/braga/redissample/RedisSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braga.redissample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
